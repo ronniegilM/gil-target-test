4211058777a444983e2ec0bc2478e0214ee56cb2
ronniegilM <ronnie.montana@bupa.com.au>
2025-07-08 18:37:45 +0800

    Update gil-test-injectscript.js

--- gil-test-injectscript.js
+++ gil-test-injectscript.js
@@ -1,2 +1,7 @@
 console.log("Js file loaded!");
 document.body.style.border = "10px solid";
+
+	const changeSignInDescription = document?.querySelector('p.my-bupa-sign-in-description');
+	if (changeSignInDescription) {
+		signInDescription.textContent = 'This description is updated from external js file!';
+	}
